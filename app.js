042bbff955dfbfce2367300387f074e653344e84
Cristianlell@gmail.com <cristianlell24@gmail.com>
2021-07-07 11:43:28 -0300

    login terminado

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.get('/',(req,res)=>{
 app.get('/register',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/register.html'))
 })
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
 
 app.listen(3010,()=>console.log('Servidor en puerto 3010'));
 
